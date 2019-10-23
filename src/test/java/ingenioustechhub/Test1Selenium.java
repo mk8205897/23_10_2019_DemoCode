29c6bd75faaa0ba34a1032780cd40dba714be1c7
mohit <mk8205897@gmail.com>
2019-10-23 06:23:21 +0530

    firstcommit

---
+++ src/test/java/ingenioustechhub/Test1Selenium.java
@@ -0,0 +1,18 @@
+package ingenioustechhub;
+
+import org.testng.annotations.Test;
+
+public class Test1Selenium {
+	
+	@Test
+	public void BrowserAutomation() {
+		System.out.println("Brwser Automation");
+		System.out.println("WebAutomation");
+	}
+ @Test
+ public void elementsUi() {
+	 System.out.println("element UI");
+ }
+	
+	
+}
---
+++ src/test/java/ingenioustechhub/TestRestAPI.java
@@ -0,0 +1,17 @@
+package ingenioustechhub;
+
+import org.testng.annotations.Test;
+
+public class TestRestAPI {
+	@Test
+	public void postJira() {
+		System.out.println("PostJira");
+		System.out.println("PostJira1");
+		System.out.println("PostJira2");
+	}
+	@Test
+	public void deleteTwwitter() {
+		System.out.println("delet Twitter");
+	}
+
+}
